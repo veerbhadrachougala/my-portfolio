72576a253b5d22c79f7d2269f959130dbaaa72a2
veerbhadrachougala <chougalav25@gmail.com>
2023-03-08 15:27:53 +0530

    final page

---
+++ src/Components/AfterSubmit.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './HomePageStyle.css';
+
+import { Link } from 'react-router-dom';
+
+const AfterSubmit = () => {
+  return (
+    <div className="hero">
+      <div className="mask">
+        <div className="mask-1"></div>
+      </div>
+      <div className="content">
+        {/* <p>You have suce</p> */}
+        <h1>Thank you...</h1>
+        <div>
+          <Link to="/" className="btn-light">
+            Back to Home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AfterSubmit;
