be03984904ea383eeecdc30da9ff6812bf4badad
veerbhadrachougala <chougalav25@gmail.com>
2023-04-15 14:01:16 +0530

    Added all in home

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import NavBar from '../Components/NavBar'
 import HomePage from '../Components/HomePage'
-import About from './About'
-import Projects from './Projects'
-import ContactMe from './Contact'
+import HeroImg from '../Components/HeroImg'
+import AboutContent from '../Components/AboutContent'
+import ProjectWork from '../Components/ProjectWork'
+import ContactForm from '../Components/ContactForm'
 import Footer from '../Components/Footer'
+
+
 
 const Home = () => {
   return (
     <div>
       <NavBar />
       <HomePage />
-      <About />
-      <Projects />
-      <ContactMe />
+      <HeroImg heading="ABOUT." text="Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology" />
+      <AboutContent />
+      <HeroImg heading="PROJECTS." text="Here you will find some of the personal projects that I have created" />
+      <ProjectWork />
+      <HeroImg heading="CONTACT." text="Feel free to Contact me by submitting the form below and I will get back to you as soon as possible" />
+      <ContactForm />
       <Footer />
     </div>
   )
